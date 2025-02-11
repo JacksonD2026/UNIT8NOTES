98a9feb5352975e63368cde6595ff52d7af793e7
jackson <jackson.dobson2026@gbwl.org>
2025-02-11 13:55:13 +0000

    2d arrat]y

--- Main.java
+++ Main.java
@@ -2,5 +2,32 @@ public class Main {
 
    public static void main(String []args) {
       System.out.println("Hello World");
+      // Declare 2D arrays [[]]
+
+      int[][] coordinates;
+      String[][] seatingChart;
+      // Create 2D array of a certain nmuber
+      // of ROWS and COLUMNS
+      coordinates = new int[5][5];
+      // default values for int : 0
+
+
+      // INITIALIZE [declare + create]
+      String[][] seatingChart = new String[3][4];
+      // default values for String : null;
+
+      // set values in a 2D array
+      seatingChart[0][0] = "Jackson";
+      seatingChart[1][0] = "Finny";
+      seatingChart[2][0] = "Bryce";
+      seatingChart[2][1] = "Natalie";
+      seatingChart[2][2] = "Paige";
+      seatingChart[2][3] = "Zoie";
+      seatingChart[0][0] = "Alex";
+      seatingChart[0][3] = "Maia";
+   
+
+      
+
    }
 }
