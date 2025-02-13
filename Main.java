d30e8f3e5138958422fa572e050e1f12a7b186f5
jackson <jackson.dobson2026@gbwl.org>
2025-02-13 13:51:54 +0000

    h

--- Main.java
+++ Main.java
@@ -38,6 +38,32 @@ public class Main {
 
       int worstSpot = minefield [0][1];
 
+      for (String[] row : seatingChart) {
+         // Inner loop iterates through column values
+         // "for every string item in the row array"
+         for (String col : row) {
+            System.out.println(col + "");
+         }
+         System.out.println(); // break between two rows
+      }
+
+      // Cannot use for-each if you want to modify values or iterate in a different order
+
+      //array.length repressents the number of rows in the 2d array
+      for (int row = 0; row < minefield.length; row++) {
+         // array[0].length repersents number of columbs in a row
+         for (int col = 0; col < minefield[0].length; col++) {
+            // if spot is a bomb replace it
+            if (minefield[row][col] < 0) {
+               minefield[row][col] = minefield[row][col] * -1;
+            }
+            System.out.print("[" + minefield[row][col] + "]");
+         }
+         System.out.println();
+      }
+
+      
+
       
 
    }
